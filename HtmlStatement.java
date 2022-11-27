2e63d7c4bc1fbe0b72c8b0f0f696b371be703131
gmiserani <60951282+gmiserani@users.noreply.github.com>
2022-11-27 23:41:56 +0000

    Commit 17

--- HtmlStatement.java
+++ HtmlStatement.java
@@ -2,20 +2,44 @@ import java.util.Enumeration;
 public class HtmlStatement extends Statement {
   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "<H1>Rentals for <EM>" + aCustomer.getName() +
-      "</EM></H1><P>\n";
+      String result = head() + aCustomer.getName() +
+      enter();
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
          //show figures for each rental
-         result += each.getMovie().getTitle()+ ": " +
-         String.valueOf(each.getCharge()) + "<BR>\n";
+         result += result(each.getMovie().getTitle()) +
+         String.valueOf(each.getCharge()) + enterBR();
       }
       //add footer lines
-      result += "<P>You owe <EM>" +
-      String.valueOf(aCustomer.getTotalCharge()) + "</EM><P>\n";
-      result += "On this rental you earned <EM>" + 
+      result += paragraph() +
+      String.valueOf(aCustomer.getTotalCharge()) + EM();
+      result += close() + 
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      "</EM> frequent renter points<P>";
+      closeP();
       return result;
    }
+  private String head(){
+    return "<H1>Rentals for <EM>";
+  }
+  private String enter(){
+    return "</EM></H1><P>\n";
+  }
+  private String result(String each){
+    return each + ": ";
+  }
+  private String enterBR(){
+    return "<BR>\n";
+  }
+  private String paragraph(){
+    return "<P>You owe <EM>";
+  }
+  private String EM(){
+    return "</EM><P>\n";
+  }
+  private String close(){
+    return "On this rental you earned <EM>";
+  }
+  private String closeP(){
+    return "</EM> frequent renter points<P>";
+  }
 }
--- TextStatement.java
+++ TextStatement.java
@@ -2,20 +2,44 @@ import java.util.Enumeration;
 public class TextStatement extends Statement {
   public String value(Customer aCustomer) {
       Enumeration rentals = aCustomer.getRentals();
-      String result = "Rental Record for " + aCustomer.getName() +
-      "\n";
+      String result = head() + aCustomer.getName() +
+      enter();
       while (rentals.hasMoreElements()) {
          Rental each = (Rental) rentals.nextElement();
          //show figures for this rental
-         result += "\t" + each.getMovie().getTitle()+ "\t" +
-         String.valueOf(each.getCharge()) + "\n";
+         result += result(each.getMovie().getTitle()) +
+         String.valueOf(each.getCharge()) + enterBR();
       }
       //add footer lines
-      result += "Amount owed is " +
-      String.valueOf(aCustomer.getTotalCharge()) + "\n";
-      result += "You earned " +
+      result += paragraph() +
+      String.valueOf(aCustomer.getTotalCharge()) + EM();
+      result += close() +
       String.valueOf(aCustomer.getTotalFrequentRenterPoints()) +
-      " frequent renter points";
+      closeP();
       return result;
    }
+  private String head(){
+    return "Rental Record for ";
+  }
+  private String enter(){
+    return "\n";
+  }
+  private String result(String each){
+    return "\t" + each + "\t";
+  }
+  private String enterBR(){
+    return "\n";
+  }
+  private String paragraph(){
+    return "Amount owed is ";
+  }
+  private String EM(){
+    return "\n";
+  }
+  private String close(){
+    return "You earned ";
+  }
+  private String closeP(){
+    return  " frequent renter points";
+  }
 }
